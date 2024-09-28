c780bc480b42cf8f71ee08151a26dcee729e05f6
qaltx <67969197+notqaltx@users.noreply.github.com>
2024-09-28 19:50:34 +0300

    Fixed some bugs.

--- index.js
+++ index.js
@@ -28,4 +28,28 @@ LoadCommands.init( client, rest, Routes );
 LoadComponents.init( client );
 LoadEvents.init( client );
 
+app.post('/check-discord-user', async (req, res) => {
+    const { username } = req.body;
+    try {
+        const guild = await client.guilds.fetch(bot.guildId);
+        const member = guild.members.cache.find(member => member.user.tag === username);
+        if (!member) {
+            return res.status(404).json({
+                errorcode: '4000',
+                message: 'User not found in the server'
+             });
+        }
+        return res.status(200).json({ 
+            errorcode: '4001',
+            message: 'User is in the server', 
+            member: member.user 
+        });
+    } catch (error) {
+        console.error('Error fetching member:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+})
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Discord Bot is listening on port " + listener.address().port);
+});
 client.login(bot.token); 
